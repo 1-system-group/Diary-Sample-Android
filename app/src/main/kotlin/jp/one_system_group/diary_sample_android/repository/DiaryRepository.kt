c06c19d3fe9756868c3fb6fa98b2f6877312dae3
birdsea <15532048+birdsea@users.noreply.github.com>
2021-10-03 13:14:25 +0900

    追加読み込み（ページング）の追加

--- app/src/main/kotlin/jp/one_system_group/diary_sample_android/repository/DiaryRepository.kt
+++ app/src/main/kotlin/jp/one_system_group/diary_sample_android/repository/DiaryRepository.kt
@@ -3,17 +3,21 @@ package jp.one_system_group.diary_sample_android.repository
 import jp.one_system_group.diary_sample_android.api.WebService
 import jp.one_system_group.diary_sample_android.model.DiaryRow
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import retrofit2.Response
 import javax.inject.Inject
 
 interface DiaryRepository {
-    suspend fun getDiaryList(): Response<List<DiaryRow>>
+    suspend fun getDiaryList(page: Int): Response<List<DiaryRow>>
 }
 
 class DiaryRepositoryImpl @Inject constructor(
     private val webService: WebService
 ) : DiaryRepository {
-    override suspend fun getDiaryList(): Response<List<DiaryRow>> =
-        withContext(Dispatchers.IO) { webService.requestDiaryList(1) }
+    override suspend fun getDiaryList(page: Int): Response<List<DiaryRow>> =
+        withContext(Dispatchers.IO) {
+            delay(3000)
+            webService.requestDiaryList(page)
+        }
 }
