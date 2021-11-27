333ddeeb627f3c0cecbf48bb59cbc5cf888165d1
birdsea <15532048+birdsea@users.noreply.github.com>
2021-11-27 22:52:31 +0900

    menuを追加

---
+++ app/src/main/kotlin/jp/one_system_group/diary_sample_android/ui/menu/DrawerScreen.kt
@@ -0,0 +1,103 @@
+package jp.one_system_group.diary_sample_android.ui.menu
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Bookmark
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import jp.one_system_group.diary_sample_android.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+sealed class DrawerListItem(val route: String, val title: String, val icon: ImageVector) {
+    object Favorite : DrawerListItem("Favorite", "お気に入り", Icons.Rounded.Favorite)
+    object Bookmark : DrawerListItem("Bookmark", "ブックマーク", Icons.Rounded.Bookmark)
+    object Setting : DrawerListItem("Setting", "設定", Icons.Rounded.Settings)
+}
+
+@Composable
+fun DrawerScreen(
+    scope: CoroutineScope,
+    scaffoldState: ScaffoldState,
+    navController: NavController
+) {
+    val items = listOf(
+        DrawerListItem.Favorite,
+        DrawerListItem.Bookmark,
+        DrawerListItem.Setting
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(colorResource(id = R.color.design_default_color_on_primary))
+
+    ) {
+        Spacer(
+            modifier = Modifier
+                .height(50.dp)
+                .width(1000.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(50.dp)
+                .height(50.dp)
+                .offset(x = 30.dp)
+                .clip(CircleShape)
+                .background(Color.DarkGray)
+        )
+        // Header
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color.White)
+                .padding(16.dp), content = {
+                Text("test@example.jp", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+                Text("20XX/01/01", color = Color.DarkGray)
+            }, verticalArrangement = Arrangement.Bottom
+        )
+        Divider(
+            modifier = Modifier
+                .height(1.dp)
+        )
+        var currentRoute = items[2].route
+        items.forEach { item ->
+            DrawerItem(item = item, selected = currentRoute == item.route, onItemClick = {
+                currentRoute = item.route
+                // Close drawer
+                scope.launch {
+                    scaffoldState.drawerState.close()
+                }
+            })
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DrawerPreview() {
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    val navController = rememberNavController()
+    DrawerScreen(scope = scope, scaffoldState = scaffoldState, navController = navController)
+}
