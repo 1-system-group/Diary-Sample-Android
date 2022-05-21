e33abcdc3519b6a3cce4308d3395942705b4f36e
birdsea <15532048+birdsea@users.noreply.github.com>
2022-05-22 06:23:56 +0900

    Top画面機能の単体テスト追加（DB）

--- app/src/test/kotlin/jp/one_system_group/diary_sample_android/repository/DiaryPagingSourceTest.kt
+++ app/src/test/kotlin/jp/one_system_group/diary_sample_android/repository/DiaryPagingSourceTest.kt
@@ -1,6 +1,7 @@
 package jp.one_system_group.diary_sample_android.repository
 
 import androidx.paging.PagingSource
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import jp.one_system_group.diary_sample_android.mock.MockDiaryRepository
 import jp.one_system_group.diary_sample_android.model.DiaryRow
 import kotlinx.coroutines.runBlocking
@@ -8,8 +9,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
-
+@Config(sdk = [31])
+@RunWith(AndroidJUnit4::class)
 class DiaryPagingSourceTest {
     private lateinit var mockRepository: MockDiaryRepository
 
@@ -21,6 +25,7 @@ class DiaryPagingSourceTest {
     @After
     fun tearDown() {
         mockRepository.close()
+
     }
 
     @Test
@@ -30,7 +35,7 @@ class DiaryPagingSourceTest {
         expectedPageKey.forEach {
             assertThat(
                 PagingSource.LoadResult.Page(
-                    data = listOf(DiaryRow(1, "テスト", "2022/01/01")),
+                    data = listOf(DiaryRow(it, "テスト", "2022/01/01")),
                     prevKey = null,
                     nextKey = it + 1 // 次のページ番号が取得できること
                 )
