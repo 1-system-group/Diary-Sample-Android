b7f194cc943fc612d483ae677842c9f3c0e25644
abe3333 <62336021+abe3333@users.noreply.github.com>
2022-07-17 14:31:36 +0900

    一覧画面から日記本文へ遷移（idは暫定で1固定）_TopAppBarを押下時に前画面に戻る

---
+++ app/src/main/kotlin/jp/one_system_group/diary_sample_android/database/ReferenceMessageDao.kt
@@ -0,0 +1,4 @@
+package jp.one_system_group.diary_sample_android.database
+
+class ReferenceMessageDao {
+}
--- app/src/main/kotlin/jp/one_system_group/diary_sample_android/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/jp/one_system_group/diary_sample_android/ui/home/HomeScreen.kt
@@ -4,12 +4,15 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
 import jp.one_system_group.diary_sample_android.model.DiaryRow
 import jp.one_system_group.diary_sample_android.viewmodel.ReferenceMessageViewModel
 import kotlinx.coroutines.launch
@@ -48,21 +51,30 @@ fun HomeScreen(
         composable(route = "reference") {
             val viewModel = hiltViewModel<ReferenceMessageViewModel>()
             val scaffoldState = rememberScaffoldState()
-            val scope = rememberCoroutineScope()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val navigationIcon: (@Composable () -> Unit)? =
+                if (navBackStackEntry?.destination?.route != "main") {
+                    {
+                        IconButton(onClick = {
+                            navController.popBackStack()
+                        }) {
+                            Icon(
+                                imageVector = Icons.Outlined.ArrowBack,
+                                contentDescription = "Back"
+                            )
+                        }
+                    }
+                } else {
+                    null
+                }
+
             Scaffold(
+
                 scaffoldState = scaffoldState,
                 topBar = {
                     TopAppBar(
                         title = { Text(viewModel.referenceMessage.title) },
-                        navigationIcon = {
-                            IconButton(
-                                onClick = {
-                                    scope.launch { scaffoldState.drawerState.open() }
-                                }
-                            ) {
-                                Icon(Icons.Filled.Close, contentDescription = null)
-                            }
-                        }
+                        navigationIcon = navigationIcon,
                     )
                 },
                 content = {
